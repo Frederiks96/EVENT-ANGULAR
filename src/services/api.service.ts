ce3cdce3da2b70e9bb115c447d0128346a07684d
Frederik <frederiks96@gmail.com>
2017-03-13 21:01:29 +0100

    Fixed Oliver's big blooper

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -9,7 +9,7 @@ export class APIService
 {
     private TOKEN_STORAGE_KEY = "api-token";
 
-    private url   : string = 'http://localhost:8080/api/';
+    private url   : string = 'http://ubuntu4.javabog.dk:3028/api/';
     private token : string = null;
 
     constructor(private http: Http, private router : Router)
