f375218cf7665b83797b652a7bc3662778c05e1a
Frederik <frederiks96@gmail.com>
2017-03-14 10:43:30 +0100

    Added jQuery and alerts

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,60 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from "@angular/forms";
-import { UserService } from "../../services/user.service";
-import { Router } from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Router} from "@angular/router";
 import {APIService} from "../../services/api.service";
 
 @Component({
-  selector: 'sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+    selector: 'sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-  private showRedAlert = false;
-  private showGreenAlert = false;
-  private alertTitle = '';
-  private alertMessage = '';
+    private showRedAlert = false;
+    private showGreenAlert = false;
+    private alertTitle = '';
+    private alertMessage = '';
 
-  user = {
-    username: 's153028',
-    password: 'dissys'
-  };
+    private isBusy = false;
 
-  constructor(private userService: UserService, private router: Router, private apiService: APIService) {
-    if (this.apiService.isAuthenticated()) {
-      console.log("User is already authenticated, navigating to home");
-      this.router.navigate(['/home']);
+    user = {
+        username: '',
+        password: ''
+    };
+
+    constructor(private router: Router, private apiService: APIService) {
+        if (this.apiService.isAuthenticated()) {
+            console.log("User is already authenticated, navigating to home");
+            this.router.navigate(['/home']);
+        }
     }
-  }
 
-  logIn(form: NgForm) {
-    this.apiService.authorize(this.user, () => {
-        this.router.navigate(['/home']);
-    }, () => {
-        this.displayRedAlert();
-    });
-  }
+    logIn(form: NgForm) {
+        this.isBusy = true;
+        this.apiService.authorize(this.user, () => {
+            this.isBusy = false;
+            this.router.navigate(['/home']);
+        }, () => {
+            this.isBusy = false;
+            this.displayRedAlert();
+        });
+    }
 
-  displayRedAlert() {
-    this.showGreenAlert = false;
-    this.alertTitle = 'Oops!';
-    this.alertMessage = 'Incorrect username or password';
-    this.showRedAlert = true;
-  }
+    displayRedAlert() {
+        this.showGreenAlert = false;
+        this.alertTitle = 'Oops!';
+        this.alertMessage = 'Incorrect username or password';
+        this.showRedAlert = true;
+    }
 
-  displayGreenAlert(username: string) {
-    this.showRedAlert = false;
-    this.alertTitle = 'Success!';
-    this.alertMessage = 'Successfully signed in: '+username;
-    this.showGreenAlert = true;
-  }
+    displayGreenAlert(username: string) {
+        this.showRedAlert = false;
+        this.alertTitle = 'Success!';
+        this.alertMessage = 'Successfully signed in: ' + username;
+        this.showGreenAlert = true;
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 
-  signup() {
+    signup() {
 
-  }
+    }
 
 }
