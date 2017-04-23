89e894af1e0f6e11fe26e19e21a46fa7276286e6
mikkelsander <mikkelguntveit@gmail.com>
2017-04-23 20:19:18 +0200

    deleted apiEvent

--- src/app/events/event-edit/event-edit.component.ts
+++ src/app/events/event-edit/event-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {ActivatedRoute, Params, Router} from "@angular/router";
 
