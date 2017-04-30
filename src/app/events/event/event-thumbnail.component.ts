0405d765ed5afafc26749ac71368e0663b217b43
Frederik <frederiks96@gmail.com>
2017-04-30 16:12:30 +0200

    General clean up

--- src/app/events/event/event-thumbnail.component.ts
+++ src/app/events/event/event-thumbnail.component.ts
@@ -6,69 +6,76 @@ import {APIService} from "../../services/api.service";
 import {Invitation} from "../event-invitations/Invitation";
 
 @Component({
-  selector: 'es-event-thumbnail',
-  templateUrl: 'event-thumbnail.component.html',
-  styleUrls: ['event-thumbnail.component.css']
+    selector: 'es-event-thumbnail',
+    templateUrl: 'event-thumbnail.component.html',
+    styleUrls: ['event-thumbnail.component.css']
 })
 export class EventThumbnailComponent implements OnInit {
 
-  @Input() event: Event;
-  private isGoing: boolean = false;
-  private invitation: Invitation;
-  private user = this.apiService.getCurrentUser();
+    @Input() event: Event;
+    private isGoing: boolean = false;
+    private invitation: Invitation;
+    private user = this.apiService.getCurrentUser();
 
 
-  constructor(private router: Router, private invitationService: InvitationService, private apiService: APIService) { }
+    constructor(private router: Router, private invitationService: InvitationService, private apiService: APIService) { }
 
+    ngOnInit() {
+        if (this.isOrganizer()) {
+            this.isGoing = true;
+            return;
+        }
+        this.fetchInvitation();
 
+    }
 
-  ngOnInit() {
-      console.log(this.event);
-      if (this.isOrganizer()) {
-          this.isGoing = true;
-          return;
-      }
-      this.fetchInvitation();
-  }
+    onGoing() {
 
-  onGoing() {
+        if (this.isOrganizer()) {
+            this.isGoing = true;
+            return;
+        }
 
-      if (this.isOrganizer()) {
-          // Current user is organizer
-          return;
-      }
+        if (this.invitation != null) {
+            // User is invited
+            if (this.invitation.isAccepted()){
+                return;
+            }
+            this.invitationService.accept(this.event.getId(), this.invitation.getInvitationID(), () => {
+                this.isGoing = true;
+                this.fetchInvitation();
+            }, () => {});
+            return;
+        }
 
-      if (this.invitation != null) {
-          // User is invited
-          if (this.invitation.isAccepted()){
-              return;
-          }
-          this.invitationService.accept(this.event.getId(), this.invitation.getInvitationID(), () => {
-              this.isGoing = true;
-              this.fetchInvitation();
-          }, () => {});
-          return;
-      }
+        if (this.event.isPublic) {
+            this.invitationService.create(this.event.getId(), this.user, (invitation) => {
+                this.isGoing = true;
+                this.fetchInvitation();
+            }, () => {} );
+        }
+    }
 
-      if (this.event.isPublic) {
-          this.invitationService.create(this.event.getId(), this.user, (invitation) => {
-              this.isGoing = true;
-              this.fetchInvitation();
-          }, () => {} );
-      }
-  }
+    private isOrganizer()
+    {
+        for (let organiser of this.event.organizers)
+        {
+            if (this.user.getID() === organiser.getID())
+            {
+                return true;
+            }
+        }
 
-  private isOrganizer() {
-      return this.event.organizers.indexOf(this.user) !== -1;
-  }
+        return false;
+    }
 
-  private fetchInvitation() {
-      this.invitationService.fetchInvitation(this.event.getId(), (invitation) => {
-          this.invitation = invitation;
-          if (this.invitation != null) {
-              this.isGoing = this.invitation.isAccepted();
-          }
-      });
-  }
+    private fetchInvitation() {
+        this.invitationService.fetchInvitation(this.event.getId(), (invitation) => {
+            this.invitation = invitation;
+            if (this.invitation != null) {
+                this.isGoing = this.invitation.isAccepted();
+            }
+        });
+    }
 
 }
